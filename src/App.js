5a9b5d120015207269b75b4271eca4f7870c12e6
Alif Hossain <alif.pab120na.12@gmail.com>
2021-12-11 17:17:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
-
+import MainContainer from './Components/MainContainer';
+import {useState} from 'react'
+import {motion}from 'framer-motion'
+import Modal from './Components/Custom/Modal';
 function App() {
+  const [isModal, setIsModal] = useState(false)
+  const [updatedLinks, setUpdatedLinks] = useState([
+    "Facebook",
+    "Instagram",
+    "Twitter",
+    "Youtube",
+    "Whatsapp",
+  ])
+   const showModal = () => {
+setIsModal(!isModal)
+   }
+
+   const modalVar = {
+     hidden: {
+       x: 0,
+       opacity: 0,
+     },
+     visible: {
+       x: 50,
+       opacity: 1,
+     },
+     exit: {
+       x: 100,
+       opacity: 0
+     },
+   }
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <MainContainer
+        showModal={showModal}
+        setUpdatedLinks={setUpdatedLinks}
+        updatedLinks={updatedLinks}
+      />
+      {isModal && (
+        <motion.div
+          // initial={{opacity: 1, x: 0}}
+          transition={{ ease: "easeOut", duration: 0.5 }}
+          variants={modalVar}
+          className="modal"
+          initial="hidden"
+          animate="visible"
         >
-          Learn React
-        </a>
-      </header>
+          <Modal
+            updatedLinks={updatedLinks}
+            setUpdatedLinks={setUpdatedLinks}
+          />
+        </motion.div>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
